38df833d8e12b85f1d351feab0e57b8d31527b79
Vladimir Popov <vladimir@dokwork.ru>
2026-08-28 19:18:41 +0400

    Add a test for poisoning

--- src/test_scenarios/periodic_effects_suite.zig
+++ src/test_scenarios/periodic_effects_suite.zig
@@ -1,29 +1,19 @@
 const std = @import("std");
-const Frame = @import("utils/Frame.zig");
+const g = @import("game");
+const c = g.components;
 const TestSession = @import("utils/TestSession.zig");
 
-test {
-    _ = @import("battle_suite.zig");
-    _ = @import("description_suite.zig");
-    _ = @import("inventory_suite.zig");
-    _ = @import("level_up_suite.zig");
-    _ = @import("moving_suite.zig");
-    _ = @import("notifications_suite.zig");
-    _ = @import("recognize_modify_suite.zig");
-    _ = @import("save_load_suite.zig");
-    _ = @import("periodic_effects_suite.zig");
-    _ = @import("trading_suite.zig");
-    _ = @import("traps_suite.zig");
-    _ = @import("update_target_suite.zig");
-}
-
-test "Hello world!" {
+test "Poison should damaging every tick" {
     var test_session: TestSession = undefined;
     try test_session.initOnFirstLevel(std.testing.allocator, std.testing.io);
     defer test_session.deinit();
+    try test_session.session.registry.set(test_session.player.id, c.Poison{ .damage = 10 });
+    const health_before = test_session.player.health().current_hp;
 
-    try test_session.tick(.{});
+    // apply poison once
+    try test_session.pressButton(.up);
 
+    // then:
     try test_session.runtime.display.expectLooksLike(
         \\######################################30
         \\#•••••••••••••#     #••••••••••••••••••#
@@ -36,10 +26,7 @@ test "Hello world!" {
         \\~~~~~~~~~~~~~~~~~~~│@│~~~~~~~~~~~~~~~~~~
         \\~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
         \\════════════════════════════════════════
-        \\                    ⇧Explore     Wait  ⇧
+        \\      Poisoned      ⇧Explore ��  Wait  ⇧
     , .whole_display);
-}
-
-test {
-    std.testing.refAllDecls(@This());
+    try std.testing.expect(test_session.player.health().current_hp < health_before);
 }
